abff012f658ae2610333d49ecbb38af919c0387a
Oscar <oscare_l@hotmail.com>
2021-11-15 17:43:32 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="tooltipcontainer">
+    <gl-search-box-by-click
+      v-model="value"
+      tooltip-container="#tooltipcontainer"
+    />
+  </div>
+</template>
+
+<script>
+import * as components from "@gitlab/ui";
+
+export default {
+  name: "EmptyState",
+  components: {
+    ...components,
+  },
+};
+</script>
